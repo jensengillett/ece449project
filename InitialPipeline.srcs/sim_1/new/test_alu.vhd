34fb3c4b7925add7f4a3bf83375f517f6adacd4f
Jensen Gillett <jensengillett@uvic.ca>
2024-03-01 21:36:24 -0800

    Improved ALU documentation, added more test cases
    -Drastically improved comments in the ALU.vhd file.
    -Added more test cases and documentation for outputs to test_alu.vhd.
    -Fixed some bugs in the TEST instruction.

--- InitialPipeline.srcs/sim_1/new/test_alu.vhd
+++ InitialPipeline.srcs/sim_1/new/test_alu.vhd
@@ -1,36 +1,20 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/26/2024 05:36:35 PM
--- Design Name: 
--- Module Name: test_alu - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
+-- Authors - Jensen Gillett, Jacob Sanders
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+-- Since this is a simulation module, we don't define any ports.
 entity test_alu is
     --  Port ( );
 end test_alu;
 
-
+-- Start architecture definition.
 architecture Behavioral of test_alu is
 
+-- Include ALU as a component.
 component alu port(
     a, b    : in    STD_LOGIC_VECTOR(15 DOWNTO 0);
     cl      : in    STD_LOGIC_VECTOR(3 DOWNTO 0);
@@ -41,6 +25,7 @@ component alu port(
 );
 end component;
 
+-- Set up signals internally for each of the ALU signals.
 signal a, b    : STD_LOGIC_VECTOR(15 DOWNTO 0);
 signal cl      : STD_LOGIC_VECTOR(3 DOWNTO 0);
 signal f       : STD_LOGIC_VECTOR(2 DOWNTO 0);
@@ -49,35 +34,88 @@ signal negative, zero, overflow : STD_LOGIC;
 signal result, extra_16_bits  : STD_LOGIC_VECTOR(15 DOWNTO 0);
 
 begin
-    u0:alu port map(a, b, cl, f, clk, negative, zero, overflow, result, extra_16_bits);
+    -- Port map the internal signals to the ALU.
+    u0:alu port map(
+        a => a,
+        b => b,
+        cl => cl,
+        f => f,
+        clk => clk,
+        negative => negative, 
+        zero => zero, 
+        overflow => overflow, 
+        result => result, 
+        extra_16_bits => extra_16_bits);
+    
+    -- Clock process. This just clocks the ALU automatically every 10us.
     process begin
         clk <= '0'; wait for 10 us;
         clk <= '1'; wait for 10 us;
     end process;
     
+    -- Test process. This feeds values into each signal to test that it matches the expected output.
     process begin
+        -- Clear signals.
         a <= (others => '0'); b <= (others => '0'); cl <= "0000"; f <= "000";
+        -- Add some delay before starting.
         wait until (clk = '0' and clk'event); wait until (clk='1' and clk'event); wait until (clk='1' and clk'event);
         wait until (clk = '1' and clk'event); 
+        -- f = 'NOP'
+        f <= "000";
+        wait until (clk = '1' and clk'event);
+        -- a = 0x0004, b = 0x0002, f = 'ADD', result should be 0x0006
         a <= (2 => '1', others => '0'); b <= (1 => '1', others => '0'); f <= "001";
         wait until (clk = '1' and clk'event);
-        a <= X"000F"; f <= "101"; cl <= "0100";
+        -- a = 0xFFFF, b = 0xFFFF, f = 'ADD', result should be 0xFFFE with overflow = 1
+        a <= X"FFFF"; b <= X"FFFF";
         wait until (clk = '1' and clk'event);
-        a <= X"FFFF"; b <= X"FFFF"; f <= "001";
-        wait until (clk = '1' and clk'event);
+        -- a = 0xFFFF, b = 0x7FFF, f = 'SUB', result should be 0x8000
         b <= X"7FFF"; f <= "010";
         wait until (clk = '1' and clk'event);
+        -- a = 0x7FFF, b = 0x0010, f = 'SUB', result should be 0x7FEF
         a <= X"7FFF"; b <= X"0010";
         wait until (clk = '1' and clk'event);
-        a <= X"7FFF"; b <= X"FFFF";
+        -- a = 0x7FFF, b = 0xFFFF, f = 'SUB', result should be 0x8000 with overflow = 1
+        b <= X"FFFF";
         wait until (clk = '1' and clk'event);
+        -- a = 0x0010, b = 0x0002, f = 'MUL', result should be 0x0020
         a <= X"0010"; b <= X"0002"; f <= "011";
         wait until (clk = '1' and clk'event);
-        a <= X"0010"; b <= X"8002";
+        -- a = 0x0010, b = 0x8002, f = 'MUL', result should be 0x8020
+        b <= X"8002";
         wait until (clk = '1' and clk'event);
+        -- a = 0x7FFF, b = 0x0002, f = 'MUL', result should be 0x0000FFFE with overflow = 1
         a <= X"7FFF"; b <= X"0002";
         wait until (clk = '1' and clk'event);
+        -- a = 0x7FFF, b = 0x8010, f = 'MUL', result should be 0x8007FFF0 with overflow = 1
         b <= X"8010";
+        wait until (clk = '1' and clk'event);
+        -- a = 0xFFFF, b = 0x0110, f = 'NAND', result should be 0xEDCB
+        a <= X"FFFF"; b <= X"1234"; f <= "100";
+        wait until (clk = '1' and clk'event);
+        -- a = 0x0123, b = 0x1234, f = 'NAND', result should be 0xFFDF
+        a <= X"0123";
+        wait until (clk = '1' and clk'event);
+        -- a = 0x000F, cl = 4, f = 'SHL', result should be 0x00F0
+        a <= X"000F"; cl <= "0100"; f <= "101"; 
+        wait until (clk = '1' and clk'event);
+        -- a = 0xF000, cl = 4, f = 'SHL', result should be 0x0000
+        a <= X"F000";
+        wait until (clk = '1' and clk'event);
+        -- a = 0xF000, cl = 4, f = 'SHR', result should be 0x0F00
+        f <= "110";
+        wait until (clk = '1' and clk'event);
+        -- a = 0x000F, cl = 4, f = 'SHR', result should be 0x0000
+        a <= X"000F";
+        wait until (clk = '1' and clk'event);
+        -- a = 0x0000, f = 'TEST', result should be zero = 1, negative = 0
+        a <= X"0000"; f <= "111";
+        wait until (clk = '1' and clk'event);
+        -- a = 0x8000, f = 'TEST', result should be zero = 1, negative = 1
+        a <= X"8000";
+        wait until (clk = '1' and clk'event);
+        -- a = 0x8001, f = 'TEST', result should be zero = 0, negative = 1
+        a <= X"8001";
         wait until (clk = '1' and clk'event);
     end process;
 end Behavioral;
--- InitialPipeline.srcs/sources_1/new/ALU.vhd
+++ InitialPipeline.srcs/sources_1/new/ALU.vhd
@@ -1,29 +1,12 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/26/2024 04:33:32 PM
--- Design Name: 
--- Module Name: ALU - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
+-- Authors - Jensen Gillett, Jacob Sanders
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+-- Declare the ALU ports as an entity.
 entity alu is
     Port(
         a, b    : in    STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -36,37 +19,44 @@ entity alu is
     );
 end alu;
 
-architecture Behavioral of alu is
-begin  
-    process (f, a, b, cl, clk)
-    variable foo    :  STD_LOGIC_VECTOR(15 DOWNTO 0) := (others => '0');
-    variable result_17 : STD_LOGIC_VECTOR(16 DOWNTO 0) := (others => '0');
-    variable result_32 : signed(31 DOWNTO 0) := (others => '0');
-    variable temp_a, temp_b : signed(15 DOWNTO 0) := (others => '0');
-    begin
-        if(clk'event and clk='1') then
-            result <= (others => '0');
-            extra_16_bits <= (others => '0');
+-- Start architecture definition.
+architecture Behavioral of alu is begin  
+    process (f, a, b, cl, clk)  -- define local inputs
+    -- Set up some internal variables used for different operations.
+    variable foo    :  STD_LOGIC_VECTOR(14 DOWNTO 0) := (others => '0');  -- 15-bit, all zeros (for testing against zero)
+    variable result_17 : STD_LOGIC_VECTOR(16 DOWNTO 0) := (others => '0');  -- Used for ADD/SUB results
+    variable result_32 : signed(31 DOWNTO 0) := (others => '0');  -- Used for MUL results
+    variable temp_a, temp_b : signed(15 DOWNTO 0) := (others => '0');  -- Used for MUL operands
+    begin -- Begin process
+        if(clk'event and clk='1') then  -- On each clock tick
+            result <= (others => '0'); -- reset result signal
+            extra_16_bits <= (others => '0');  -- reset MUL extra bits
+            negative <= '0'; zero <= '0'; overflow <= '0';  -- reset flags
             case f is
-                when "000" =>
-                    -- NOP
-                when "001" => 
-                    result_17 := std_logic_vector(signed(a(15) & a) + signed(b(15) & b));  -- add
+                when "000" =>  -- NOP
+                    -- No inputs, no outputs.
+                when "001" =>  -- ADD
+                    -- Perform signed addition. We sign-extend a and b beforehand to get the correct 17-bit result with overflow.
+                    result_17 := std_logic_vector(signed(a(15) & a) + signed(b(15) & b));  
+                    result <= result_17(15 DOWNTO 0);  -- Store the 16-bit true result.
+                    overflow <= result_17(16);  -- Store overflow bit.
+                when "010" =>  -- SUB
+                    -- Perform signed subtraction. See addition above.
+                    result_17 := std_logic_vector(signed(a(15) & a) - signed(b(15) & b)); 
                     result <= result_17(15 DOWNTO 0);
-                    overflow <= result_17(16);
-                when "010" =>
-                    result_17 := std_logic_vector(signed(a(15) & a) - signed(b(15) & b));  -- sub
-                    result <= result_17(15 DOWNTO 0);
-                    -- Overflow bit is set consistently wrong if one operand is negative; fix it
+                    -- Overflow bit is set consistently wrong if one operand (but not both) is negative; this if/else fixes the overflow bit.
                     if((a(15) = '1' and b(15) = '0') or (a(15) = '0' and b(15) = '1')) then
                         overflow <= not result_17(16);
                     else
                         overflow <= result_17(16);
                     end if;
-                when "011" => 
-                    -- mul
+                when "011" => -- MUL
+                    -- Amazingly, this actually completes in one cycle, meaning all operations in this ALU complete in one cycle.
+                    -- This makes things nice when this is integrated with the rest of the system.
                 
                     -- We remove the negatives from the original operands before multiplication.
+                    -- Multiplication results are always provably positive or negative dependant on operands.
+                    -- If we don't do this, the sign extension we'd have to do later screws all the calculations up.
                     if(a(15) = '1' and b(15) = '1') then --two negatives
                         temp_a := signed('0' & a(14 DOWNTO 0));
                         temp_b := signed('0' & b(14 DOWNTO 0));
@@ -81,34 +71,38 @@ begin
                         temp_b := signed(b(15 DOWNTO 0));
                     end if;
                     
-                    -- multiplication
+                    -- Perform signed multiplication of two 16-bit variables into a 32-bit variable.
                     result_32 := signed(temp_a) * signed(temp_b);
                     
                     -- If the result should have been negative, make it negative, otherwise make it positive.
+                    -- (This is doing the inverse of what we did before the multiplication)
                     if((a(15) = '1' and b(15) = '1') or (a(15) = '0' and b(15) = '0')) then
                         result_32(31) := '0';
                     else
                         result_32(31) := '1';
                     end if;
-                    --report "The value of 'result_32' is " & integer'image(to_integer(result_32));
-                    -- If the result would overflow a 16-bit value, handle accordingly
-                    if(result_32(30 DOWNTO 15) /= X"0000") then
+                    
+                    -- If the result would overflow a 16-bit value, we raise the overflow flag and store
+                    -- the extra bits into the 'extra_16_bits' signal.
+                    -- Otherwise, we just store the 16-bit value.
+                    if(result_32(30 DOWNTO 15) /= X"0000") then  -- 32-bit values have sign at position 31. We check the next 16 values.
                         result <= std_logic_vector(result_32(15 DOWNTO 0));
                         extra_16_bits <= std_logic_vector(result_32(31 DOWNTO 16));
                         overflow <= '1';
-                    else  -- 16-bit value fits in register
+                    else  -- 16-bit value fits in the regular output register.
                         result <= std_logic_vector(result_32(31) & result_32(14 DOWNTO 0));
                         overflow <= '0';
                     end if;
-                when "100" => 
-                    result <= a NAND b;  -- nand
-                when "101" => 
-                    result <= std_logic_vector(shift_left(unsigned(a), to_integer(unsigned(cl)))); -- shl
-                when "110" =>
-                    result <= std_logic_vector(shift_right(unsigned(a), to_integer(unsigned(cl)))); -- shr
-                when others =>  -- test
+                when "100" =>  -- NAND
+                    result <= a NAND b;  
+                when "101" =>  -- SHL (Shift Left)
+                    -- Both SHL and SHR use the built-in NUMERIC_STD operators, but they require type conversion of a and cl to unsigned and integer, respectively.
+                    result <= std_logic_vector(shift_left(unsigned(a), to_integer(unsigned(cl))));
+                when "110" =>  -- SHR (Shift Right)
+                    result <= std_logic_vector(shift_right(unsigned(a), to_integer(unsigned(cl))));
+                when others =>  -- TEST
                     -- test for zero                    
-                    if(a = foo) then
+                    if(a(14 DOWNTO 0) = foo) then
                         zero <= '1';
                     else
                         zero <= '0';
@@ -117,7 +111,7 @@ begin
                     if(a(15) = '1') then
                         negative <= '1';
                     else
-                        negative <= '1';
+                        negative <= '0';
                     end if;
             end case;
         end if;
